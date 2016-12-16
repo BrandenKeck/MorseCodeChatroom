c0f3e0c2779d3323ddeb049b392563dbef8a0e00
Branden Keck <bmkeck62@gmail.com>
2016-12-16 10:55:34 -0500

    Fixed Resizing for Mobile

--- login.js
+++ login.js
@@ -8,10 +8,10 @@ function setUser(){
 	if(userGlobal === undefined || userGlobal == ""){userGlobal = "Anon" + setRandom;}
 	
 	$("#skippy1").empty();
-	$("#skippy1").append('<div class="col-xs-12"><h3 class="subtitle">Start Typing...</h3><textarea id="IO" class="col-xs-9"></textarea></div>');
+	$("#skippy1").append('<div class="col-xs-12"><h3 class="subtitle">Start Typing...</h3><textarea id="IO"></textarea></div>');
 
 	$("#skippy2").empty();
-	$("#skippy2").append('<div class="col-xs-5"></div><div class="col-xs-2"><button value="Send" id="sendButton" tabIndex="-1" onclick="send()"><h6>Send</h6></Button></div><div class="col-xs-5"></div>');
+	$("#skippy2").append('<div class="col-xs-4 col-sm-5"></div><div class="col-xs-4 col-sm-2"><button value="Send" id="sendButton" tabIndex="-1" onclick="send()"><h6>Send</h6></Button></div><div class="col-xs-4 col-sm-5"></div>');
 
 	setInterval(function(){
 		$("#IO").scrollTop($("#IO")[0].scrollHeight);
